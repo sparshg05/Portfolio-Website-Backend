f8f0c04aaffc4cca38c2f0a8859cea4eccc6dcfb
Sparsh Goyal <sparshg05@gmail.com>
2023-01-08 00:49:00 +0530

    server.js updated

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 import { app} from "./app.js";
 import dotenv from "dotenv";
-// import {connectDatabase} from "./config/database.js";
+import {connectDatabase} from "./config/database.js";
 import cloudinary from "cloudinary";
-import mongoose from 'mongoose';
 
 const port = process.env.PORT || 4000;
 
 dotenv.config({path: "./backend/config/config.env"});
 
-// connectDatabase();
-mongoose.connect("mongodb+srv://mesparshg05:befearless@clusters.r9q9z.mongodb.net/portfolio?retryWrites=true" )
-.then(c=>{
-    console.log(`Mongodb connect to: ${c.connection.host}`);
-}).catch(e=>{
-    console.log(e);
-})
+connectDatabase();
 
 cloudinary.v2.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
