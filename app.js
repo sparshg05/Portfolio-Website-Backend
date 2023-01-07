3d94439728ae2a3734f7c596a8f7593e0b3eb030
Sparsh Goyal <sparshg05@gmail.com>
2023-01-08 00:30:39 +0530

    cors headers added

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ app.use(express.json({ limit:"50mb" }));
 app.use(express.urlencoded({extended:true, limit:"50mb"}));
 app.use(cookieParser());
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Methods","GET,PUT,POST,DELETE,PATCH,OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization");
+    next();
+})
+
 
 
 import {userRouter} from "./routes/User.js";
